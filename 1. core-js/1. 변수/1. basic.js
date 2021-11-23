c1cb60f5222c60a086ceaa359f3b33cde650b6f3
leejaesueng <glduss@naver.com>
2021-11-23 18:45:32 +0900

    js_basic

---
+++ 1. core-js/1. 변수/1. basic.js
@@ -0,0 +1,33 @@
+//특정 연산의 결과를 저장하고 싶어요
+
+//1. 변수를 만든다 (선언)
+var result;
+
+//2. 변수를 초기화 (값을 저장)
+result = 10 + 12;
+
+console.log(result * 3);
+
+result = result * 3;
+
+console.log(result);
+
+//3. 변수는 선언과 동시에 초기화 가낭
+var score = 95;
+
+console.log(score);
+
+score = 73;
+
+console.log(score);
+
+score = result + 10;
+
+console.log(score);
+
+console.log('=============================');
+
+var user_name = '고길동';
+
+console.log(`안녕하세요! 제 이름은 ${user_name}입니다`);
+console.log(`${user_name}은 아주 착합니다.`);
